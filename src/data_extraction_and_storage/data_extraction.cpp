a1d02f4ad4cbcb036d7dadb1ebae86f3385610b0
doufaman <st199686@stud.uni-stuttgart.de>
2026-04-20 23:46:21 +0200

    first

--- src/data_extraction_and_storage/data_extraction.cpp
+++ src/data_extraction_and_storage/data_extraction.cpp
@@ -2,7 +2,7 @@
 #include "config.h" //global config
 #include <iostream>
 
-// 仅在此文件实现函数
+// 仅在此文件实现
 namespace DataExtraction {
     void extractFromPBF(const std::string& pbf_path) {
         std::cout << "Reading: " << pbf_path << "\n";
