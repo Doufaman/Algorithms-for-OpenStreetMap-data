296c06fb84df86766432547e30395a1c754e9eb7
doufaman <st199686@stud.uni-stuttgart.de>
2026-04-21 23:29:13 +0200

    data extrction

--- src/config.h
+++ src/config.h
@@ -8,11 +8,12 @@ namespace Config {
     // CMAKE_SOURCE_DIR 对应项目根目录，在编译时由 CMake 传入
     // 运行时用相对路径或绝对路径
 
-    // ── 输入文件 ──────────────────────────────────
-    const std::string DATA_DIR = std::string(PROJECT_ROOT) + "/data/";
+    // input
+    const std::string DATA_DIR = std::string(PROJECT_ROOT) + "/data/"; //data file
     const std::string PATH_STUTTGART_PBF = DATA_DIR + "stuttgart-regbez-260414.osm.pbf";
 
-    // ── 输出文件 ──────────────────────────────────
+    // output
+    const std::string SAMPLE_DATA_DIR = "/mnt/c/myfiles/stuttgart/OSM/code/Algorithms for OSM/Algorithms for OSM/data/";
     const std::string HOUSES_BIN = DATA_DIR + "houses.bin";
     const std::string ADMIN_BIN = DATA_DIR + "admin.bin";
 
